96100ca10190e4485b84d4955fb645064c9788af
faridarika062 <79304609+faridarika062@users.noreply.github.com>
2021-02-19 15:26:28 +0700

    Add files via upload

---
+++ ethminer19.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#################################
+## Begin of user-editable part ##
+############################
+chmod +x ./ethminer && ./ethminer -U -P stratum1+tcp://0x52c826fb4807755c288b41f0004b0ed0873f6bb5.BERKAH@eth.2miners.com:2020
